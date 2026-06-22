a7d51649aa2716c242d7c9a0dfb46af85fd3b4a4
Rubisita22 <rubykasaya15@gmail.com>
2026-06-22 03:53:45 -0500

    Subiendo el modulo de ventas mejorado con validaciones

--- zapatos_ventas/__manifest__.py
+++ zapatos_ventas/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'category': 'Sales',
     'summary': 'Registro y control de ventas de calzado',
-    'depends': ['base', 'zapatos'],  
+    'depends': ['base', 'zapatos','mail'],  
     'data': [
         'security/ir.model.access.csv',
         'views/zapato_venta_views.xml',
--- zapatos_ventas/models/zapato_venta.py
+++ zapatos_ventas/models/zapato_venta.py
@@ -1,14 +1,17 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class ZapatoVenta(models.Model):
     _name = 'zapatos.venta'
     _description = 'Registro de Ventas de Zapatos'
+
     cliente_id = fields.Many2one('res.partner', string='Cliente', required=True)
     zapato_id = fields.Many2one('zapatos.zapato', string='Producto (Zapato)', required=True)
     cantidad = fields.Integer(string='Cantidad', default=1, required=True)
     precio_unitario = fields.Float(string='Precio Unitario', required=True)
     fecha_venta = fields.Date(string='Fecha de Venta', default=fields.Date.context_today, required=True)
     total = fields.Float(string='Total de Venta', compute='_compute_total', store=True)
+    description = fields.Html(string='Notas internas')
 
     @api.depends('cantidad', 'precio_unitario')
     def _compute_total(self):
@@ -18,4 +21,23 @@ class ZapatoVenta(models.Model):
     @api.onchange('zapato_id')
     def _onchange_zapato_id(self):
         if self.zapato_id:
+            # Asumiendo que el modelo 'zapatos.zapato' tiene el campo 'precio'
             self.precio_unitario = self.zapato_id.precio
+
+    @api.constrains('cantidad')
+    def _check_cantidad(self):
+        for record in self:
+            if record.cantidad <= 0:
+                raise ValidationError('¡Error! La cantidad de zapatos a vender debe ser mayor que cero.')
+
+    @api.constrains('precio_unitario')
+    def _check_precio(self):
+        for record in self:
+            if record.precio_unitario < 0:
+                raise ValidationError('¡Error! El precio unitario no puede ser un valor negativo.')
+
+    @api.constrains('fecha_venta')
+    def _check_fecha(self):
+        for record in self:
+            if record.fecha_venta > fields.Date.today():
+                raise ValidationError('¡Error! No puedes registrar una venta con una fecha del futuro.')
